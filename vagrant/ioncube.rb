ba555e79f4331230648702c6ca6e5825f24e2505
DJ Walker <dwalker@d3corp.com>
2016-07-07 09:06:11 -0400

    add ioncube installer

---
+++ vagrant/ioncube.rb
@@ -0,0 +1,19 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# requires vagrant-triggers
+# requires vagrant-hostmanager
+
+$script = <<SCRIPT
+
+cd ~
+sudo rm -rf Ioncube-Autoinstall
+git clone https://github.com/Chikoumi/Ioncube-Autoinstall
+sudo chmod +x Ioncube-Autoinstall/eng_ioncube.sh
+sudo ./Ioncube-Autoinstall/eng_ioncube.sh
+
+SCRIPT
+
+Vagrant.configure("2") do |config|   
+    config.vm.provision "shell", inline: $script
+end
