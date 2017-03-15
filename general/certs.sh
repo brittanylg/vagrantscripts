6182b29d6f8fde2ea8b75709dfd1c9e6977d7aea
dwalkr <superdeer@gmail.com>
2017-03-15 15:00:20 -0400

    Update certs.sh

--- general/certs.sh
+++ general/certs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo openssl req -new -x509 -days 365 -nodes -out /etc/ssl/certs/ssl.crt -keyout /etc/ssl/private/ssl.key -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=www.example.ro"
+
 sudo service apache2 restart
