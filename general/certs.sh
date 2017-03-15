999ef53230b7101d3875f88dba204d2006d033bd
Nick Yli-Piipari <nickyp@d3corp.com>
2017-03-15 14:39:58 -0400

    Updates to Certs task
    Added sudo to certs command and refactored command to a single line. Added 'certs' task to ExampleVagrantFile

--- general/certs.sh
+++ general/certs.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-openssl req -new -x509 -days 365 -nodes \
-    -out /etc/ssl/certs/ssl.crt \
-    -keyout /etc/ssl/private/ssl.key \
-    -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=www.example.ro"
-
-
+sudo openssl req -new -x509 -days 365 -nodes -out /etc/ssl/certs/ssl.crt -keyout /etc/ssl/private/ssl.key -subj "/C=RO/ST=Bucharest/L=Bucharest/O=IT/CN=www.example.ro"
 sudo service apache2 restart
