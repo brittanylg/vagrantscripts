ffa31045916ebf7c3107be7ad7f2796110632e4a
DJ <dwalker@d3corp.com>
2018-02-08 12:37:58 -0500

    pin scotchbox version

--- provision/bedrock.rb
+++ provision/bedrock.rb
@@ -27,16 +27,17 @@ SCRIPT
 
 Vagrant.configure("2") do |config|
     config.vm.box = "scotch/box"
+    config.vm.box_version = "2.5"
     config.vm.network "private_network", type: "dhcp"
     config.vm.hostname = "#{$hostname}"
-    
+
     config.vm.synced_folder ".", "#{$vm_path}", :mount_options => ["dmode=777", "fmode=775"]
     config.vm.synced_folder "#{$path_to_scripts}/general", "/home/vagrant/scripts", :mount_options => ["dmode=777", "fmode=775"]
     config.vm.synced_folder ".", "/vagrant", disabled: true
     config.vm.box_download_insecure = true
 
     config.vm.provision "shell", inline: script
-    
+
     config.trigger.after [:up, :reload] do
         run_remote "bash /home/vagrant/scripts/startup-scotchbox.sh"
         run "composer self-update"
